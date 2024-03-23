e197f54b0a572e29fca6f07a7db422d9e5680f0f
Brian Yeap <bryeap@u81z06s04.42kl.edu.my>
2024-03-23 17:06:34 +0800

    Fix

--- ex01/ft_strncmp.c
+++ ex01/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: bryeap <bryeap@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/22 23:06:34 by brian             #+#    #+#             */
-/*   Updated: 2024/03/23 14:57:12 by bryeap           ###   ########.fr       */
+/*   Updated: 2024/03/23 17:05:25 by bryeap           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,19 @@ int	ft_strncmp(char *s1, char *s2, unsigned int n)
 {
 	unsigned int	i;
 
+	if (n <= 0)
+		return (0);
+	i = 0;
 	while (s1[i] == s2[i] && s1[i] && i < n - 1)
 		i++;
 	if (s1[i] == s2[i])
 		return (0);
 	return (s1[i] - s2[i]);
 }
+
+// #include <stdio.h>
+// int main()
+// {
+// 	printf("%d", ft_strncmp("", "fsfdsf", 0));
+// 	return (0);
+// }
